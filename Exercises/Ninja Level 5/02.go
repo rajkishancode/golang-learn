0a80a7d38b0a2b078f6851c1bace2b31cd16a95c
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2024-05-15 15:15:44 +0530

    good

---
+++ Exercises/Ninja Level 5/02.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+
+type person struct{
+	first string
+	last string
+	favFlavors []string
+}
+
+func main(){
+	p1 := person{
+		first: "James",
+		last: "Bonds",
+		favFlavors: []string{"vanilla","strawberry"},
+	}
+
+	m := map[string]string{
+		
+	}
+
+}
+
