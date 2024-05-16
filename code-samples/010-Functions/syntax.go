c0d3f0a85aaf30ef79dc87892ce47e18accd4f66
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2024-05-16 11:50:36 +0530

    function syntax in go

---
+++ code-samples/010-Functions/syntax.go
@@ -0,0 +1,33 @@
+package main
+import "fmt"
+
+func main(){
+	foo()
+	bar("James")
+	s:= woo("Moneypenny");
+	fmt.Println(s);
+	x,y := fullName("Ian","Flemings")
+	fmt.Println(x)
+	fmt.Println(y)
+}
+
+func foo(){
+	fmt.Println("Hello from foo")
+}
+//EVERYTHING IN GO IS PASS BY VALUE
+func bar(s string){ // takes an argument
+  fmt.Println("Hello from",s)
+}
+
+// function return a string
+func woo(s string)string{
+	return fmt.Sprint("Hello from ,",s);  //Sprint is string print
+}
+
+//function with multiple returns
+func fullName(first string,last string) (string, bool) {
+	a := fmt.Sprint(first,"  ",last ,"  ",`say have a nice day!`);
+	b := false
+	return a,b
+}
+
