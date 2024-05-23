81fa7c3ee4d97915e4c05b97c260a7c2866569ee
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2024-05-23 17:19:50 +0530

    go handson

--- Exercises/Ninja-Level-9/06-os-arch/main.go
+++ Exercises/Ninja-Level-9/06-os-arch/main.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"runtime"
 )
+//Hands-on-Exercise #6
 
+/*
+Create a Program that Prints out your OS and ARCH . Use the following command to run it
+	- go run
+	- go build - will create a executable file same folder
+	- go install - will move exe file to C:\Users\Raj\Documents\goworkspace\bin
+				 - go to the bin path from vscode terminal 
+				 - just type file name ex- 06-os-arch > it will run .
+*/
 func main() {
 	fmt.Println("OS", runtime.GOOS)
 	fmt.Println("ARCH", runtime.GOARCH)
--- Exercises/Ninja-Level-9/07/main.go
+++ Exercises/Ninja-Level-9/07/main.go
@@ -1,5 +1,41 @@
 package main
 import "fmt"
-
+//IF YOU DONT LEAVE YOUR COMFORT ZONE , YOU WILL NOT REMEMBER THE TRIP
+// If we do something out of our comfort zone we REMEMBER it and REMEMBER = LEARNING
+// ONE OF THE BEST WAY OF LEARNING TO DO SOMETHING OUTSIDE OUR COMFORT ZONE
+// There are 7 Billion people in the world and its okay TO BE HUMAN and NOT TO BE PERFECT 
 //Hands-on-Exercise #7
+/*
+	Download OBS. Create a screen recording teaching some aspect of the Go programming
+	language. Some topics you might teach:
+	 - Why Go ?
+	 - installing go
+	 - GOROOT & GOPATH
+	 	> environment variables
+	 - hello world
+	 - go commands
+	  	> help
+	 - variables
+	 	- Short declaration operator
+	 - constants
+	 - loops
+	    > init, cond ,post
+	    > break
+	    > continue
+	 - functions
+	 	> func (receiver) identifier(params) (returns) {code}
+	 - methods
+	 - interfaces
+	 - method sets
+	 - type
+	    > conversion
+	    > concurrency vs parallelism
+	    > goroutines
+	    > waitgroups
+	    > mutex
+	    > atomic
+
+AFTER CREATING YOUR RECORDING UPLOAD YOUR VIDEO TO YOUTUBE
+THEN TWEET A LINK TO YOUR VIDEO AND TAG ME IN THE TWEET SO THAT TODD CAN SEE IT.@Todd_McLeod
+*/
 func main(){}
